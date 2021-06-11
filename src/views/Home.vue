5be3b95067f27d2e218c52d1573286a330f41ff3
Koji Ono <onokoji@syngram.co.jp>
2021-06-11 18:03:03 +0900

    さらにテスト

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Koji's App"/>
+    <HelloWorld msg="Welcome to Koji Ono's App"/>
   </div>
 </template>
 
