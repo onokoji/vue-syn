a18fe57df3e45d136bb070b914d068f9aabb997e
Koji Ono <onokoji@syngram.co.jp>
2021-06-16 16:17:21 +0900

    XDデザインを入れ込み中

--- src/main.js
+++ src/main.js
@@ -4,4 +4,11 @@ import router from './router'
 import store from './store'
 import AnimateCss from 'animate.css';
 // Install Animate.css
-createApp(App).use(store).use(router).mount('#app').use(AnimateCss)
+//ここからfontawesome追加
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faLeaf } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faLeaf)
+//ここまで追加
+
+createApp(App).use(store).use(router).mount('#app').use(AnimateCss).component('font-awesome-icon', FontAwesomeIcon )
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="home page">
 <img src="../assets/logo-line.svg" alt="">
-<h1>2021シングラム文化祭はじまる</h1>
+<h1>2021シングラム文化祭はじまる<font-awesome-icon icon="spinner" size="xs" /></h1>
 
 <p class="home">今回はVue.jsを使ってWEBサイトを作ってみました。<a href="https://qiita.com/567000/items/dde495d6a8ad1c25fa43" target="_blank">vue-cliをインストール</a>  するところから始めて、いくつかのサイトで画面遷移のエフェクトをテストしつつ、<a href="https://v3.ja.vuejs.org/guide/transitions-enterleave.html" target="_blank">公式サイトでEnter & Leave トランジション</a> を確認したところです、どうぞよろしく。</p>
 </div>
