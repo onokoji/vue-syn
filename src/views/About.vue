dc0ae5b8c09d9717f5dd0ebd14070cab6b34cad7
Koji Ono <onokoji@syngram.co.jp>
2021-06-16 18:00:19 +0900

    animete.cssも追加

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="about page">
 <img src="../assets/logo-y.svg" alt="">
-<h1>昨年の自作ロゴを使って。<router-link to="/thanks"><font-awesome-icon icon="arrow-circle-right" /></router-link> </h1>
+<h1>昨年の自作ロゴを使って。<router-link to="/thanks"><font-awesome-icon icon="arrow-circle-right" class="animated headShake infinite" /></router-link> </h1>
 <p>Vue.jsには transition という機能があるのですね、これを利用したシンプルなエフェクトを入れてあります。ちょっとやり始めるといろいろと調べないとできないですね、まずは触りで、Vue.jsをさわれたのはよかったです。</p>
 </div>
 </template>
